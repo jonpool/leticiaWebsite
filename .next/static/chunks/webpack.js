69de59388f6c339b7694a5ea7c2242600e008097
jonpool <joaopl_4@hotmail.com>
2025-08-17 15:29:46 -0400

    titleFixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9812713062912238"; }
+/******/ 		__webpack_require__.h = function() { return "a8d5b3b939d84c9a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
