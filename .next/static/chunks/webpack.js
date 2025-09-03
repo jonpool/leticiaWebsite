b0f8ce2751cd90dac04ee359a371fd257afb79a3
jonpool <joaopl_4@hotmail.com>
2025-09-03 19:07:58 -0400

    firstPage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a5a08c5300f3a64c"; }
+/******/ 		__webpack_require__.h = function() { return "f7ea999c3b6ca16f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
