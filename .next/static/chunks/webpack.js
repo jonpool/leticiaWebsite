f3ffbbceeaa4d2381115853e6fccefc0d9f7776a
jonpool <joaopl_4@hotmail.com>
2025-09-03 18:58:34 -0400

    titleandDescription

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f1fe54558a3400f5"; }
+/******/ 		__webpack_require__.h = function() { return "a5a08c5300f3a64c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
